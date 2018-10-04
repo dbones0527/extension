42351e644d18fd22cf971d009a7f66b6b99594c6
Anton Bershanskiy <bershan2@illinois.edu>
2018-10-04 15:45:31 -0500

    Minor fix in script injection

--- source/common/content_scripts/inject.js
+++ source/common/content_scripts/inject.js
@@ -4,8 +4,6 @@
 	// Execute the actal payload in the context of the window
 	var s = document.createElement("script");
 	s.src = chrome.extension.getURL("/web_accessible_resources/inject.js");
-	s.onload = function() {
-		this.remove();
-	}
 	(document.head || document.documentElement).appendChild(s);
+	s.parentNode.removeChild(s);
 })()

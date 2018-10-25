f50e35212c1d34fb6a357eb73206639e05afd290
Anton Bershanskiy <bershan2@illinois.edu>
2018-10-24 21:32:42 -0500

    minor refactoring

--- source/common/pages/popup/popup.js
+++ source/common/pages/popup/popup.js
@@ -2,49 +2,48 @@
 
 const mainMenu = document.getElementById("main-menu")
 
+const mainMenuMonitoring = document.getElementById ("main-menu-monitoring")
+const mainMenuSharing = document.getElementById ("main-menu-sharing")
+const mainMenuProtection = document.getElementById ("main-menu-protection")
+
+const detailsDefault = document.getElementById ("details-default")
+
 const detailsMonitoring = document.getElementById ("details-monitoring")
 const detailsSharing = document.getElementById ("details-sharing")
 const detailsProtection = document.getElementById ("details-protection")
-
 
 /*
  * For debug purposes only. This displays the "message" within the UI.
  */
 function debugMessage(message){
-	var div = document.createElement("DIV");
-	div.innerText = message;
-	document.body.appendChild(div);
+	var div = document.createElement("DIV")
+	div.innerText = message
+	document.body.appendChild(div)
 }
 
-debugMessage("hi");
-
-
 document.addEventListener("DOMContentLoaded", function() {
-	debugMessage("hi");
 
 	mainMenu.addEventListener("click", function(evt) {
 		var elem = evt.target
 		while (elem !== undefined && elem !== null){
 			if (elem.classList !== undefined && elem.classList.contains("main-menu-item")){
 				// Found the main-menu-item
-				detailsMonitoring.style.display = "none"
-				detailsSharing.style.display = "none"
-				detailsProtection.style.display = "none"
-				const id = elem.id
-				switch (id){
-					case "main-menu-monitoring":
-						detailsMonitoring.style.display = "block"
-						break
-					case "main-menu-sharing":
-						detailsSharing.style.display = "block"
-						break
-					case "main-menu-protection":
-						detailsProtection.style.display = "block"
-						break
-				}
-				debugMessage("found" + id)
+				mainMenuMonitoring.classList.remove("main-menu-item--active")
+				mainMenuSharing.classList.remove("main-menu-item--active")
+				mainMenuProtection.classList.remove("main-menu-item--active")
+				elem.classList.add("main-menu-item--active")
+
+				detailsDefault.classList.remove("details-item--active")
+				detailsMonitoring.classList.remove("details-item--active")
+				detailsSharing.classList.remove("details-item--active")
+				detailsProtection.classList.remove("details-item--active")
+
+				const selection = elem.id.substring("main-menu-".length)
+				document.getElementById ("details-" + selection).classList.add("details-item--active")
+				break
+			} else {
+				elem = elem.parentNode
 			}
-			elem = elem.parentNode
 		}
 	})
 })

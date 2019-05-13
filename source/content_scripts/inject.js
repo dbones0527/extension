b74a62ca28aef63771bfe6132eb4b7ae5025f118
Anton Bershanskiy <bershan2@illinois.edu>
2019-05-13 09:44:12 -0500

    f

--- source/content_scripts/inject.js
+++ source/content_scripts/inject.js
@@ -1,10 +1,14 @@
 // This code is executed in a separate context with "clean DOM"
 
+const prefix = "chrome-extension://ahebiabmadaookaondjagfaedejgedlc"
+
 // Execute the actal payload in the context of the window
-const s = document.createElement("script")
-s.src = chrome.extension.getURL("/web_accessible_resources/inject.js")
-(document.head || document.documentElement).appendChild(s)
-s.parentNode.removeChild(s)
+const script = document.createElement("SCRIPT")
+script.src = () => {return prefix + "/web_accessible_resources/inject.js" }
+(document.head || document.documentElement).appendChild(script)
+script.parentNode.removeChild(script)
+
+console.info("content script executed")
 
 document.addEventListener("cookie_message", function(event) {
   // We only accept messages from ourselves

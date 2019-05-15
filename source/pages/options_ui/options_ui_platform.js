1c5135564f56b329106d237e4c0621cbf70fe8c1
Anton Bershanskiy <bershan2@illinois.edu>
2019-05-14 23:53:35 -0500

    s

--- source/pages/options_ui/options_ui_platform.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-const platform = chrome
-
-function originIsDisplayable(origin){
-  return !origin.startsWith("moz-extension:\/\/")
-}
--- source/pages/options_ui/options_ui_platform_firefox.js
+++ source/pages/options_ui/options_ui_platform_firefox.js
@@ -1,5 +1,7 @@
 "use strict"
 
+const platform = chrome
+
 function originIsDisplayable(origin){
   debugMessage(origin + "stuff" )
   return !origin.startsWith("moz-extension:\/\/")
